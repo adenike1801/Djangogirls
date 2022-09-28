6d333a517a13d89e42f242c5b9087ea33c7633f6
adenike1801 <aoriolowo1@ivytech.edu>
2022-09-28 14:18:04 -0400

    Add HTML and views

--- myvenv/Scripts/activate.bat
+++ myvenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Adenike Oriolowo\Desktop\Adenike\SDEV\djangogirls\myvenv
+set VIRTUAL_ENV=C:\Users\Adenike Oriolowo\Desktop\Adenike\SDEV\Djangogirls\myvenv
 
 if not defined PROMPT set PROMPT=$P$G
 
